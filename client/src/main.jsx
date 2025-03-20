bc9ad9bd33aa5a26c42de9098d9a3493353ecc7c
DEV0110010 <newera.deepanshu@gmail.com>
2025-03-20 14:34:43 +0530

    react persist formed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,15 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <BrowserRouter>
+   <PersistGate persistor={persistor}>
    <Provider store={store}>
     <App />
     </Provider>
+    </PersistGate>
     </BrowserRouter>
   </StrictMode>,
 )
