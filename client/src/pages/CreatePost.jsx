8f6b49fd2570d2ac12ecbd8d92daf8afb02b4414
DEV0110010 <newera.deepanshu@gmail.com>
2025-03-30 14:40:44 +0530

    complete create post fuctionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,63 +1,108 @@
-import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react'
-import React, { useState } from 'react'
-import ReactQuill from 'react-quill-new';
-import 'react-quill-new/dist/quill.snow.css';
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import { app } from '../firebase';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+import { Alert, Button, FileInput, Select, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+    const navigate = useNavigate();
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageUploadProgress] = useState(null);
-    const [imageUploadError, setImageUploadError] = useState(null)
-    const [formData, setFormData] = useState({})
+    const [imageUploadError, setImageUploadError] = useState(null);
+    const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    
     const handleUploadImage = async () => {
         try {
             if (!file) {
-                setImageUploadError('Please select an image');
+                setImageUploadError("Please select an image");
                 return;
             }
             setImageUploadError(null);
             const storage = getStorage(app);
-            const fileName = new Date().getTime() + '-' + file.name;
+            const fileName = new Date().getTime() + "-" + file.name;
             const storageRef = ref(storage, fileName);
             const uploadTask = uploadBytesResumable(storageRef, file);
             uploadTask.on(
-                'state_changed',
+                "state_changed",
                 (snapshot) => {
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    const progress =
+                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                     setImageUploadProgress(progress.toFixed(0));
                 },
                 (error) => {
-                    setImageUploadError('Image upload failed');
-                    setImageUploadProgress(null)
+                    setImageUploadError("Image upload failed");
+                    setImageUploadProgress(null);
                 },
                 () => {
                     getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
                         setImageUploadProgress(null);
                         setImageUploadError(null);
-                        setFormData({...formData, image: downloadURL})
-                    })
+                        setFormData({ ...formData, image: downloadURL });
+                    });
                 }
-            )
-
+            );
         } catch (error) {
-           setImageUploadError('Image upload failed');
-           setImageUploadProgress(null);
+            setImageUploadError("Image upload failed");
+            setImageUploadProgress(null);
+        }
+    };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch ('/api/post/create',{
+                method: 'POST',
+                headers:{
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+              });
+              const data = await res.json();
+              if(!res.ok){
+                setPublishError(data.message)
+                return
+              }
+    
+              if(res.ok){
+                setPublishError(null);
+                navigate(`/post/${data.slug}`);
+              }
+        } catch (error) {
+            setPublishError('Something went wrong');
         }
     }
 
-
     return (
-        <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-            <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
-            <form className='flex flex-col gap-4'>
-                <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                    <TextInput type='text' placeholder='Title' required id='title' className='flex-1' />
+        <div className="p-3 max-w-3xl mx-auto min-h-screen">
+            <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                <div className="flex flex-col gap-4 sm:flex-row justify-between">
+                    <TextInput
+                        type="text"
+                        placeholder="Title"
+                        required
+                        id="title"
+                        className="flex-1"
+                        onChange={(e) => {
+                          setFormData({ ...formData, title: e.target.value })
+                        }
+                        }
+                    />
 
-                    <Select>
+                    <Select onChange={(e) => {
+                          setFormData({ ...formData, category: e.target.value })
+                        }
+                        }>
                         {/* General Category */}
                         <option value="uncategorized">Select a category</option>
 
@@ -69,7 +114,9 @@ const CreatePost = () => {
                         <option value="cybersecurity">Cybersecurity</option>
                         <option value="data-science">Data Science</option>
                         <option value="machine-learning">Machine Learning</option>
-                        <option value="artificial-intelligence">Artificial Intelligence</option>
+                        <option value="artificial-intelligence">
+                            Artificial Intelligence
+                        </option>
                         <option value="cloud-computing">Cloud Computing</option>
                         <option value="blockchain">Blockchain</option>
                         <option value="software-development">Software Development</option>
@@ -81,7 +128,9 @@ const CreatePost = () => {
                         <option value="devops">DevOps</option>
                         <option value="frontend-development">Frontend Development</option>
                         <option value="backend-development">Backend Development</option>
-                        <option value="full-stack-development">Full Stack Development</option>
+                        <option value="full-stack-development">
+                            Full Stack Development
+                        </option>
                         <option value="database-management">Database Management</option>
                         <option value="software-testing">Software Testing</option>
                         <option value="quantum-computing">Quantum Computing</option>
@@ -90,7 +139,9 @@ const CreatePost = () => {
                         <option value="virtual-reality">Virtual Reality</option>
                         <option value="augmented-reality">Augmented Reality</option>
                         <option value="web3">Web3</option>
-                        <option value="cyber-physical-systems">Cyber-Physical Systems</option>
+                        <option value="cyber-physical-systems">
+                            Cyber-Physical Systems
+                        </option>
                         <option value="game-ai">Game AI</option>
                         <option value="edge-computing">Edge Computing</option>
 
@@ -137,10 +188,14 @@ const CreatePost = () => {
                         <option value="resume-building">Resume Building</option>
                         <option value="time-management">Time Management</option>
                         <option value="career-switching">Career Switching</option>
-                        <option value="soft-skills-development">Soft Skills Development</option>
+                        <option value="soft-skills-development">
+                            Soft Skills Development
+                        </option>
                         <option value="hybrid-work">Hybrid Work</option>
                         <option value="conflict-resolution">Conflict Resolution</option>
-                        <option value="entrepreneurship-mindset">Entrepreneurship Mindset</option>
+                        <option value="entrepreneurship-mindset">
+                            Entrepreneurship Mindset
+                        </option>
 
                         {/* Art & Creativity */}
                         <option value="art">Art</option>
@@ -153,10 +208,14 @@ const CreatePost = () => {
                         <option value="stoicism">Stoicism</option>
                         <option value="productivity-hacks">Productivity Hacks</option>
                         <option value="digital-detox">Digital Detox</option>
-                        <option value="emotional-intelligence">Emotional Intelligence</option>
+                        <option value="emotional-intelligence">
+                            Emotional Intelligence
+                        </option>
                         <option value="gratitude-practices">Gratitude Practices</option>
                         <option value="life-coaching">Life Coaching</option>
-                        <option value="personal-finance-for-students">Personal Finance for Students</option>
+                        <option value="personal-finance-for-students">
+                            Personal Finance for Students
+                        </option>
                         <option value="lifestyle">Lifestyle</option>
                         <option value="self-improvement">Self-Improvement</option>
                         <option value="personal-development">Personal Development</option>
@@ -232,7 +291,9 @@ const CreatePost = () => {
                         <option value="biohacking">Biohacking</option>
                         <option value="strength-training">Strength Training</option>
                         <option value="fitness-gadgets">Fitness Gadgets</option>
-                        <option value="diet-nutrition-science">Diet & Nutrition Science</option>
+                        <option value="diet-nutrition-science">
+                            Diet & Nutrition Science
+                        </option>
                         <option value="home-workouts">Home Workouts</option>
                         <option value="mental-resilience">Mental Resilience</option>
 
@@ -257,40 +318,60 @@ const CreatePost = () => {
                     </Select>
                 </div>
                 <div className="flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3">
-                    <FileInput type='file' accept='image/*' onChange={(e) => {
-                        setFile(e.target.files[0])
-                    }
-                    } />
-                    <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline 
-                    onClick={handleUploadImage} 
-                    disabled={imageUploadProgress}>
-                        {
-                        imageUploadProgress ? (
-                            <div className='w-16 h-16'>
-                                    <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`}/>
-                            </div>   
+                    <FileInput
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => {
+                            setFile(e.target.files[0]);
+                        }}
+                    />
+                    <Button
+                        type="button"
+                        gradientDuoTone="purpleToBlue"
+                        size="sm"
+                        outline
+                        onClick={handleUploadImage}
+                        disabled={imageUploadProgress}
+                    >
+                        {imageUploadProgress ? (
+                            <div className="w-16 h-16">
+                                <CircularProgressbar
+                                    value={imageUploadProgress}
+                                    text={`${imageUploadProgress || 0}%`}
+                                />
+                            </div>
                         ) : (
-                            'Upload Image'
-                        )
-                    }
+                            "Upload Image"
+                        )}
                     </Button>
                 </div>
-                {imageUploadError && 
-                    <Alert color='failure'>
-                            {imageUploadError}
-                    </Alert>}
-                    {formData.image && (
-                        <img src={formData.image} 
-                        alt='upload'
-                        className='h-72 w-full object-cover'/>
-                    )}
-                <ReactQuill theme='snow' placeholder='Write something' className='h-72 mb-12' required />
-                <Button type='submit' gradientDuoTone='purpleToPink'>
+                {imageUploadError && <Alert color="failure">{imageUploadError}</Alert>}
+                {formData.image && (
+                    <img
+                        src={formData.image}
+                        alt="upload"
+                        className="h-72 w-full object-cover"
+                    />
+                )}
+                <ReactQuill
+                    theme="snow"
+                    placeholder="Write something"
+                    className="h-72 mb-12"
+                    required
+                    onChange={(value) => {
+                        setFormData({ ...formData, content: value })
+                      }
+                      }
+                />
+                <Button type="submit" gradientDuoTone="purpleToPink" className="mt-4 sm:mt-0">
                     Publish
                 </Button>
+                {
+                    publishError && <Alert className="mt-5" color='failure'>{publishError}</Alert>
+                }
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default CreatePost
+export default CreatePost;
