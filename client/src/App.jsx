ac88a21bb1589112737493800437c416f6454421
DEV0110010 <newera.deepanshu@gmail.com>
2025-04-08 11:53:08 +0530

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,10 +13,12 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePOst'
 import PostPage from './pages/PostPage'
+import ScrollToTop from './components/ScrollToTop'
 
 const App = () => {
   return (
     <>
+    <ScrollToTop/>
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>} />
